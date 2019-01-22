f555355966167806de18d6fa5b0b14504f529e7f
Jurgen <info@jold.nl>
2019-01-22 17:59:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# jQuery JOLD Js-block
+
+Version 1.0.0
+
+## Summary
+
+A jQuery plugin that shows placeholder images for embedded YouTube or Vimeo videos, and only loads the video after clicking the placeholder.
+
+## Author
+
+Jurgen Oldenburg ( [@staxxnl](http://twitter.com/staxxnl) / [jold.nl](https://www.jold.nl) / [info@jold.nl](info@jold.nl) )
+
+## Usage
+
+Include `jquery.jold.js-load-video.min.js` after calling jQuery in the footer. Alternatively, include in your `plugins.js` file if using [HTML5 Boilerplate](http://html5boilerplate.com).
+
+
+### Installation
+
+```bash
+$ npm install jquery.jold.js-load-video
+
+```
+
+
+### HTML Markup
+
+```html
+<div class="video-container">
+    <div class="js-load-video" data-service="vimeo" data-placeholder="" data-embed="306834650">
+        <a href="#" class="btn btn__large btn__green btn__notext btn__modal--play" title="Video afspelen"></a>
+    </div>
+</div>
+
+```
+
+Set the data attributes:
+
+**Service:** Can be either YouTube or Vimeo. Other services are not supported.
+
+**placeholder:** You can provide your own placeholder (url). Leave this empty to retrieve the thumbnail from YouTube or Vimeo automatically.
+
+**data-embed:** The video key or ID from YouTube or Vimeo
+
+
+
+
+### Initialize the plugin
+
+```js
+$('.js-load-video').joldJsLoadVideo({ youtubeThumbSize: 'hqdefault', vimeoThumbSize: 'thumbnail_large' });
+
+```
+
+This example checks for all .js-load-video elements, places a placeholder and loads the video on click.
+
+
+
+
+## Requirements
+
+Tested with jQuery 1.12.1.
+
+
+## Changelog
+
+
+#### Version 1.0.0
+
+* Initial commit.
---
+++ jquery.jold.js-load-video.js
@@ -0,0 +1,121 @@
+/*!
+ * JOLD jQuery Load Video 1.0.0
+ *
+ * Copyright (c) 2018 JOLD Interactive; Jurgen Oldenburg <info@jold.nl>
+ *
+ * A jQuery plugin that only loads youtube and vimeo video's after clicking, for better performance
+ * It also loads a placeholder image
+ * See README.md for usage
+ *
+ * Licences: BSD-3-Clause
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+(function($){
+
+    var JoldLoadVideo = function( element, options ) {
+
+        var obj         = this;
+        var $element    = $(element);
+        var defaults    = {
+            youtubeThumbSize: 'maxresdefault',
+            vimeoThumbSize: 'thumbnail_large',
+        }
+
+        /**
+         * Pick up the options passed to the plugin
+         */
+        var settings = $.extend({}, defaults, options);
+
+        /**
+         * Load video's on click
+         */
+        $element.each(function() {
+
+            // Get all data from video
+            var service     = $(this).data('service');
+            var placeholder = $(this).data('placeholder');
+            var videoKey    = $(this).data('embed');
+
+            // Prepare placeholder image DOM element
+            var image       = new Image();
+
+            // Set image attributes
+            $(image).addClass('responsive-img');
+
+            // Prepare the iframe
+            var iframe      = document.createElement('iframe');
+
+            // Set iframe attributes
+            $(iframe).attr('webkitallowfullscreen', '');
+            $(iframe).attr('mozallowfullscreen', '');
+            $(iframe).attr('webkitallowfullscreen', '');
+            $(iframe).attr('frameborder', '0');
+            $(iframe).attr('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+
+
+            // Get uploaded placeholder, if its present
+            if ( placeholder !== ''  ) {
+                image.src   = placeholder;
+            }
+
+
+            // YouTube placeholder and iframe url
+            if ( service == 'youtube' ) {
+                image.src   = "https://img.youtube.com/vi/" + videoKey + "/" + settings.youtubeThumbSize + ".jpg";
+                iframe.src  = "https://www.youtube.com/embed/" + videoKey + "?rel=0&showinfo=0&autoplay=1";
+            }
+
+            // Vimeo placeholder and iframe url
+            else if ( service == 'vimeo' ) {
+
+                $.ajax({
+                    url: "http://vimeo.com/api/v2/video/" + videoKey + ".json",
+                    dataType: 'jsonp',
+                    success: function(data){
+                        image.src = data[0][settings.vimeoThumbSize];
+                    }
+                });
+
+                iframe.src  = "https://player.vimeo.com/video/" + videoKey + "?autoplay=true";
+
+            }
+
+            // Actually append the image to the DOM
+            $(this).append( image );
+
+            // Render the video embed on click
+            $(this).on('click', function(event) {
+
+                event.preventDefault();
+
+                // Clear out video container
+                $(this).html('');
+
+                // Add the iframe
+    			$(this).append( iframe );
+
+            });
+
+        });
+
+    };
+
+
+    $.fn.joldLoadVideo = function( options ) {
+
+        var element = $(this);
+
+        // Return early if this element already has a plugin instance
+        if (element.data('joldLoadVideo')) return element.data('joldLoadVideo');
+
+        // pass options to plugin constructor
+        var joldLoadVideo = new JoldLoadVideo( this, options );
+
+        // Store plugin object in this element's data
+        element.data('joldLoadVideo', joldLoadVideo);
+
+        return joldLoadVideo;
+
+    };
+
+})(jQuery);
